3c9c5e69ddb7ca7d2059fb4ca4c4e5fbc9688d15
Rohit <Rohits0591@gmail.com>
2026-05-13 19:32:18 +0530

    modified private key format

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"|"$/g, ''),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     clientId: process.env.FIREBASE_CLIENT_ID,
     clientX509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
